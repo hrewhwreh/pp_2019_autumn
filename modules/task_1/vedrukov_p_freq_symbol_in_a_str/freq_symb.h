f4cb1dfc3e17c8d3ba1ac1ed803ef3292dd4ac9b
hrewhwreh <pavelvedrukov@mail.ru>
2019-10-21 00:27:31 +0400

    frequency_s_in_a_str

---
+++ modules/task_1/vedrukov_p_freq_symbol_in_a_str/freq_symb.h
@@ -0,0 +1,10 @@
+// Copyright 2019 Vedrukov Pavel
+#ifndef MODULES_TASK_1_VEDRUKOV_P_FREQUENCY_SYMBOL_IN_A_STRING_H_
+#define MODULES_TASK_1_VEDRUKOV_P_FREQUENCY_SYMBOL_IN_A_STRING_H_
+
+#include <iostream>
+#include <string>
+
+double getFreq(std::string str, char symb);
+
+#endif  // MODULES_TASK_1_VEDRUKOV_P_FREQUENCY_SYMBOL_IN_A_STRING_H_
