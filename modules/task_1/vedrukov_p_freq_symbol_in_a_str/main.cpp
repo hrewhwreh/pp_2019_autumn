f07c219aebb76b1208de8f5cbe388abf7235a1e6
hrewhwreh <pavelvedrukov@mail.ru>
2019-10-21 23:31:56 +0400

    fix errors 3

--- modules/task_1/vedrukov_p_freq_symbol_in_a_str/main.cpp
+++ modules/task_1/vedrukov_p_freq_symbol_in_a_str/main.cpp
@@ -11,8 +11,9 @@ TEST(freq_symb, zero_freq) {
     std::string str = "ChargeOfDarkness";
     char symb = '0';
     double freq = getFreq(str, symb);
-    if (rank == 0)
+    if (rank == 0) {
         ASSERT_EQ(0, freq);
+    }
 }
 
 TEST(freq_symb, full_freq) {
@@ -21,8 +22,9 @@ TEST(freq_symb, full_freq) {
     std::string str = "oooooooo";
     char symb = 'o';
     double freq = getFreq(str, symb);
-    if (rank == 0)
+    if (rank == 0) {
         ASSERT_EQ(1, freq);
+    }
 }
 
 TEST(count_frequency, some_freq_1) {
@@ -31,8 +33,9 @@ TEST(count_frequency, some_freq_1) {
     std::string str = "pool";
     char symb = 'o';
     double freq = getFreq(str, symb);
-    if (rank == 0)
+    if (rank == 0) {
         ASSERT_EQ(0.5, freq);
+    }
 }
 
 TEST(count_frequency, some_freq_2) {
@@ -41,8 +44,9 @@ TEST(count_frequency, some_freq_2) {
     std::string str = "pointclick";
     char symb = 'i';
     double freq = getFreq(str, symb);
-    if (rank == 0)
+    if (rank == 0) {
         ASSERT_EQ(0.2, freq);
+    }
 }
 
 TEST(count_frequency, some_freq_3) {
@@ -51,8 +55,9 @@ TEST(count_frequency, some_freq_3) {
     std::string str = "limbobimbo";
     char symb = 'b';
     double freq = getFreq(str, symb);
-    if (rank == 0)
+    if (rank == 0) {
         ASSERT_EQ(0.3, freq);
+    }
 }
 
 int main(int argc, char** argv) {
